8e85582bac6980261620c7c44161d207c83ffe62
”alenclover55” <alenclover@yandex.ru>
2024-10-27 09:02:06 +0300

    Добавление модалок

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,46 +1,26 @@
-// function showPopup(e) {
-//     let event = new Event("modalshow")
-//     window.dispatchEvent(event)
-//     setTimeout(() => {
-//         $(".popup").is(".active") && $(".popup").removeClass("active"),
-//         $(".overlayed").removeClass("opacity-0 invisible"),
-//         $("body").addClass("overflow-hidden"),
-//         $(".popup." + e).removeClass("hidden invisible"),
-//         setTimeout(()=>{
-//             $(".popup." + e).addClass("active")
-//         }
-//         , 100)
-//     }, 300)
-// }
-// function closePopup(e) {
-//     $(".popup").is(".active") && $(".popup").removeClass("active"),
-//     $(".popup." + e).addClass("closed"),
-//     $(".overlayed").addClass("opacity-0 invisible"),
-//     $("body").removeClass("overflow-hidden"),
-//     $(".popup." + e).addClass("hidden invisible").removeClass("closed")
-// }
-// $(document).ready(function() {
-//     $(".popup__close").click(function(e) {
-//         return $(".popup").addClass("closed"),
-//         setTimeout(()=>{
-//             $(".overlayed").addClass("opacity-0 invisible"),
-//             $("body").removeClass("overflow-hidden"),
-//             $(".popup").addClass("hidden invisible").removeClass("closed active")
-//         }
-//         , 300),
-//         !1
-//     }),
-//     $(".overlayed").click(function(e) {
-//         (e.target || e.srcElement).className.search("overlay") || ($(".popup").addClass("closed"),
-//         setTimeout(()=>{
-//             $(".overlayed").addClass("opacity-0 invisible"),
-//             $("body").removeClass("overflow-hidden"),
-//             $(".popup").addClass("hidden invisible").removeClass("closed active")
-//         }
-//         , 300))
-//     }),
-//     $("[rel=popup]").click(function(e) {
-//         return showPopup($(this).attr("data-popup")),
-//         !1
-//     })
-// });
+function showPopup(selector) {
+  $(".overlayed").addClass("!opacity-100 !visible pointer-events-auto");
+  $(selector).addClass("!block !visible");
+}
+$(".popup__close").click(() => {
+  $(".overlayed").removeClass("!opacity-100 !visible pointer-events-auto");
+  $(".popup").removeClass("!block !visible");
+});
+
+$("#sign-in-btn").click(function () {
+  showPopup(".popup--sign-in");
+});
+
+$("#sign-up-btn").click(function () {
+  showPopup(".popup--sign-up");
+});
+
+$("#wallet-modal").click(function () {
+  showPopup(".popup--wallet");
+});
+$("#level-popup-btn").click(function () {
+  showPopup(".levels_popup");
+});
+$("#cashback-modal-btn").click(function () {
+  showPopup(".popup--cashback");
+});
